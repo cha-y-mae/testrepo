4e1e2936737e7c84fd266d2347352e0cc498415b
cha-y-mae <107033012+cha-y-mae@users.noreply.github.com>
2022-10-23 15:48:31 +0400

    Update Hero.js

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -2,7 +2,6 @@ import React from 'react'
 import './HeroStyles.css'
 // import {AiOutlineSearch} from 'react-icons/ai'
 
-import video from '../../assets/maldivesVideo.mp4' 
 import pic from '../../assets/finalversionpic.jpg' 
 
 function Hero() {
