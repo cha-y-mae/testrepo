62662c296ea9c568fd488a3161eb2850b1711921
cha-y-mae <107033012+cha-y-mae@users.noreply.github.com>
2022-10-23 15:49:29 +0400

    Update Selects.js

--- src/components/selects/Selects.js
+++ src/components/selects/Selects.js
@@ -1,12 +1,6 @@
 import React from 'react'
 import './SelectsStyles.css'
 
-import BoraBora from '../../assets/borabora.jpg'
-import BoraBora2 from '../../assets/borabora2.jpg'
-import Maldives from '../../assets/maldives.jpg'
-import Maldives2 from '../../assets/maldives2.jpg'
-import Maldives3 from '../../assets/maldives3.jpg'
-import KeyWest from '../../assets/keywest.jpg'
 
 import SelectsImg from '../SelectsImg/SelectsImg'
 
@@ -15,12 +9,7 @@ function Selects() {
     return (
         <div name='views' className='selects'>
             <div className='container'>
-                <SelectsImg bgImg={BoraBora} text='Bora Bora' />
-                <SelectsImg bgImg={BoraBora2} text='Emerald Bay' />
-                <SelectsImg bgImg={Maldives} text='Maldives' />
-                <SelectsImg bgImg={Maldives2} text='Grenada' />
-                <SelectsImg bgImg={Maldives3} text='Barbados' />
-                <SelectsImg bgImg={KeyWest} text='Key West' />
+               
             </div>
 
         </div>
