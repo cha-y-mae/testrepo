3a1a130cb359bfc0d68381b6f6b732cd370659fe
cha-y-mae <107033012+cha-y-mae@users.noreply.github.com>
2022-10-23 15:47:21 +0400

    Update agenda.js

--- src/components/agenda/agenda.js
+++ src/components/agenda/agenda.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './agenda.css'
 
-import BoraBora from '../../assets/borabora.jpg'
+/*import BoraBora from '../../assets/borabora.jpg'
 import BoraBora2 from '../../assets/borabora2.jpg'
 import Maldives from '../../assets/maldives.jpg'
 import Maldives2 from '../../assets/maldives2.jpg'
-import KeyWest from '../../assets/keywest.jpg'
+import KeyWest from '../../assets/keywest.jpg'*/ 
 
 function agenda() {
     return (
