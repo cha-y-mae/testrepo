6f5eecb970164222bdd8f228fafe2457637606e3
cha-y-mae <107033012+cha-y-mae@users.noreply.github.com>
2022-11-17 00:29:55 +0400

    Update agenda.js

--- src/components/agenda/agenda.js
+++ src/components/agenda/agenda.js
@@ -9,6 +9,11 @@ function agenda() {
          
             <p className="welcome-text"> If you would like to attend the workshop, you can request an invitation by filling out <a style={{fontWeight: 'bold', color:'black'}} href="https://docs.google.com/forms/d/e/1FAIpQLSevSyer8QbS-VhC4hnitdOHnHznQ4lnzq9qCYRXg-zxKhIATA/viewform?vc=0&c=0&w=1&flr=0">this form.</a></p>
             <br></br>
+
+   <h1>Acknowledgements</h1>
+
+                    <p className="welcome-text"> We would like to thank Chaimae Abouzahir for creating and managing the website and the NYU Abu Dhabi Engineering Division for sponsoring the workshop </p>
+
     </div>
     )
 }
